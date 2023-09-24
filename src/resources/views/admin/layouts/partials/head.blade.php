3f4c3c72bc0e443b3c93c45ba11c961492f5070a
TranDat1811 <transidat1998@gmail.com>
2023-09-24 10:25:03 +0700

    1002 layout admin

---
+++ src/resources/views/admin/layouts/partials/head.blade.php
@@ -0,0 +1,7 @@
+<link rel="shortcut icon" href="favicon.ico"> 
+    
+<!-- FontAwesome JS-->
+<script defer src="{{ asset('themes/assets/plugins/fontawesome/js/all.min.js') }}"></script>
+
+<!-- App CSS -->  
+<link id="theme-style" rel="stylesheet" href="{{ asset('themes/assets/css/portal.css') }}">
